389a4d7c3c8324c19dc6fcbb4ed9ed7f568c36ee
Dhanush1137 <dhanushahane01@gmail.com>
2022-03-08 09:18:21 +0530

    Task-2 Add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+
+    // Sidebar Toggler
+    $('.sidebar-toggler').click(function () {
+        $('.sidebar, .content').toggleClass("open");
+        return false;
+    });
+    
+})(jQuery);
+
